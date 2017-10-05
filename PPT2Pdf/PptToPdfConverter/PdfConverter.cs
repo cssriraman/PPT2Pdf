c2d2414c6f178e9e23586d14f3c46aadf72301ec
Sriraman <cssriraman@gmail.com>
2017-10-05 15:46:30 +0530

    Add files via upload

---
+++ PPT2Pdf/PptToPdfConverter/PdfConverter.cs
@@ -0,0 +1,53 @@
+﻿using Spire.Presentation;
+using System;
+using System.IO;
+
+namespace PptToPdfConverter
+{
+    public class PdfConverter
+    {
+        private readonly string OutDir;
+
+        public PdfConverter(string outDir)
+        {
+            this.OutDir = outDir;
+        }
+
+
+        public bool Process(string inFilePath)
+        {
+            try
+            {
+                var fi = new FileInfo(inFilePath);
+                var fileName = fi.Name;
+                var fileBaseName = Path.GetFileNameWithoutExtension(inFilePath);
+                var ext = Path.GetExtension(inFilePath);
+                var outPdf = Path.Combine(OutDir, $"{fileBaseName}.pdf");
+
+                Console.Write($"Processing {fileName} ... ");
+
+                if (ext != ".ppt" && ext != ".pptx")
+                {
+                    Console.WriteLine("Invalid filetype.");
+                    return false;
+                }
+
+                // Load the document
+                var ppDoc = new Presentation();
+                ppDoc.LoadFromFile(inFilePath);
+                // Save the document in output format.
+                ppDoc.SaveToFile(outPdf, FileFormat.PDF);                
+
+                Console.WriteLine("Completed.");
+
+                return true;
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+                return false;
+            }
+        }
+    }
+
+}
